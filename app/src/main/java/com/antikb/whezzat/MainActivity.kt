eba359e7ca8db4ad12b660d1523c8c1f10c59dc7
Antik <theantikim@gmail.com>
2022-12-29 14:19:08 +0530

    initial commit

---
+++ app/src/main/java/com/antikb/whezzat/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.antikb.whezzat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
