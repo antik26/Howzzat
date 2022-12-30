0d5ef5468bbad3c0b996a8f10a7b197a774c3963
Antik <theantikim@gmail.com>
2022-12-30 20:42:35 +0530

    secondlayout

---
+++ app/src/main/java/com/antikb/whezzat/LeagueActivity.kt
@@ -0,0 +1,11 @@
+package com.antikb.whezzat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LeagueActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_league)
+    }
+}
